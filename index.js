c386de641697d5460f1e8f2317b31f1b11ad0a42
luannsct <luan.nsct@gmail.com>
2023-05-01 22:58:50 -0300

    Projeto node

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const fs = require('fs');
+
+const hostname = "127.0.0.1";
+
+const port = 3000;
+
+const server = http.createServer((req,res)=>{
+    fs.readFile('index.html',function(err,data){
+        console.log(req.headers);
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    })
+});
+
+server.listen(port,hostname,()=>{
+    console.log("servidor está on-line");
+});
