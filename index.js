82fbfce2e093f4ed790df1e72631c685373b12f7
luannsct <luan.nsct@gmail.com>
2023-05-01 23:13:24 -0300

    Criando e manipulando arquivos com node

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const http = require('http');
-
 const fs = require('fs');
 
 const hostname = "127.0.0.1";
-
 const port = 3000;
+
+//criar novo arquivo
+// fs.writeFile('teste.txt','Teste de Escrita em arquivo',(err)=>{
+//     if(err) throw err;
+//     console.log('O arquivo foi criado com sucesso!')
+// })
+//cria novo arquivo ou insere conteudo depois do que já existe
+
+fs.appendFile('teste.txt',"\nOutro Conteúdo!",(err)=>{
+    if (err) throw err;
+    console.log('Nova linha salva com sucesso!');
+})
 
 const server = http.createServer((req,res)=>{
     fs.readFile('index.html',function(err,data){
